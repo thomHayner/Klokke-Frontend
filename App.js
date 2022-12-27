ce950f541b344295ed8c1d548132a3122ee22cea
Thomas Hayner <thom.hayner@gmail.com>
2022-12-27 04:28:39 -0700

    add safe-area-context support

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
