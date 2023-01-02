7474228a4ab08aeb1ad308d874bc5240b319e921
Thomas Hayner <thom.hayner@gmail.com>
2023-01-02 08:00:13 -0700

    reorganize and style component

--- components/screens/timers/Timer.js
+++ components/screens/timers/Timer.js
@@ -13,96 +13,156 @@ import {
   deleteTimer,
   resetTimer,
   handleCompleteTimer,
+  renameTimer,
+  moveToNewList,
 } from './timersFunctions';
 
 export default function Timer({ timer, timers, setTimers, serverTimestamp }) {
-  const { HH, MM, SS } = displayProperTime(timer, serverTimestamp)
+  const { HH, MM, SS } = displayProperTime(timer, serverTimestamp);
+
+  const TimerActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: [
+          'Delete Timer',
+          'Reset Timer',
+          timer.completed ? 'Unlock Timer' : 'Mark Task Completed',
+          'Rename Timer',
+          'Move To List',
+          'Close Menu',
+        ],
+        cancelButtonIndex: 5,
+        destructiveButtonIndex: 0,
+      },
+      buttonIndex => {
+        if (buttonIndex === 0) {deleteTimer(timer, timers, setTimers)};
+        if (buttonIndex === 1) {resetTimer(timer, timers, setTimers)};
+        if (buttonIndex === 2) {
+          handleCompleteTimer(timer, timers, setTimers)
+        };
+        if (buttonIndex === 3) {renameTimer};
+        if (buttonIndex === 4) {moveToNewList};
+      }
+    )
+  };
+
   return (
-    <Pressable onPress={() => handleTimer(timer, timers, setTimers, serverTimestamp)}>
-      <View style={styles.timerWrapper}>
-        <View style={styles.innerCompartment}>
-          {timer.completed ? 
-            <MaterialIcons name="lock-outline" size={35} color="black" />
-          :
-            <MaterialIcons name="lock-open" size={35} color="black" />
-          }
+    <View style={[styles.container, styles.row, styles.timerOuterWrapper]}>
+      <Pressable 
+        style={[styles.container, styles.mainCompartment]}
+        onPress={() => handleTimer(timer, timers, setTimers, serverTimestamp)}
+      >
+        <View style={styles.mainCompartmentTop}>
+          <Text style={[styles.row, styles.timerName]}>
+            {timer.name}
+          </Text>
+          <Text style={[styles.row, styles.listName]}>
+            List: {timer.list ? timer.list : 'none'}
+          </Text>
         </View>
 
-        <View style={styles.innerCompartment}>
-          <View style={styles.row}>
-            <Text style={styles.clockface}>{HH}</Text>
-            <Text style={styles.clockfaceDivider}>:</Text>
-            <Text style={styles.clockface}>{MM}</Text>
-            <Text style={styles.clockfaceDivider}>:</Text>
-            <Text style={styles.clockface}>{SS}</Text>
+        <View style={[styles.row, styles.mainCompartmentBottom]}>
+          <View style={[styles.container, ]}>
+            {timer.completed ? 
+              <MaterialIcons
+                name="lock-outline"
+                size={30}
+                color="#249DFF"
+              />
+            :
+              timer.isRunning ? 
+                <MaterialIcons
+                  name="pause-circle-outline"
+                  size={30}
+                  color="#FF2824"
+                />
+              :
+                <MaterialIcons
+                  name="play-circle-outline"
+                  size={30}
+                  color="#389916"
+                />
+            }
           </View>
-          {/* <Text style={styles.clockface}>{displayProperTime(timer, serverTimestamp)}</Text> */}
-          {/* <Text style={styles.clockHelper}>HH:MM:SS</Text> */}
-          <View style={styles.row}>
-            <Text style={styles.clockfaceLabel}>h</Text>
-            <Text style={styles.clockfaceLabelDivider}> </Text>
-            <Text style={styles.clockfaceLabel}>m</Text>
-            <Text style={styles.clockfaceLabelDivider}> </Text>
-            <Text style={styles.clockfaceLabel}>s</Text>
+          <View style={styles.clockContainer}>
+            <View style={styles.row}>
+              <Text style={styles.clockface}>{HH}</Text>
+              <Text style={styles.clockfaceDivider}>:</Text>
+              <Text style={styles.clockface}>{MM}</Text>
+              <Text style={styles.clockfaceDivider}>:</Text>
+              <Text style={styles.clockface}>{SS}</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.clockfaceLabel}>h</Text>
+              <Text style={styles.clockfaceLabelDivider}> </Text>
+              <Text style={styles.clockfaceLabel}>m</Text>
+              <Text style={styles.clockfaceLabelDivider}> </Text>
+              <Text style={styles.clockfaceLabel}>s</Text>
+            </View>
           </View>
         </View>
+      </Pressable>
 
-        <View style={styles.innerCompartment}>
-          <Text>{timer.name}</Text>
-          <Text>List: {timer.list ? timer.list : 'none'}</Text>
-        </View>
-
-        <View style={styles.innerCompartment}>
-          <Pressable onPress={() => {
-            ActionSheetIOS.showActionSheetWithOptions(
-              {
-                options: [
-                  'Delete Timer',
-                  'Reset Timer',
-                  timer.completed ? 'Unlock Timer' : 'Mark Task Completed',
-                  'Rename Timer',
-                  'Close Menu',
-                ],
-                cancelButtonIndex: 4,
-                destructiveButtonIndex: 0,
-              },
-              buttonIndex => {
-                if (buttonIndex === 0) {deleteTimer(timer, timers, setTimers)};
-                if (buttonIndex === 1) {resetTimer(timer, timers, setTimers)};
-                if (buttonIndex === 2) {handleCompleteTimer(timer, timers, setTimers)};
-                if (buttonIndex === 3) {};
-              }
-            )
-          }}>
+        <Pressable
+          style={[styles.container]}
+          onPress={TimerActionSheet}>
+        <View style={[styles.container, styles.menuIconContainer]}>
             <MaterialIcons name="menu" size={24} color="black" />
-          </Pressable>
         </View>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   )
 };
 
 const styles = StyleSheet.create({
-  timerWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  timerOuterWrapper: {
     minHeight: 100,
     borderRadius: 8,
     borderWidth: 1,
     marginVertical: 2,
-    paddingHorizontal: 8,
-    paddingVertical: 2,
+    backgroundColor: '#E9F5FF'
   },
-  innerCompartment: {
+  mainCompartment: {
+    flex: 9,
+    alignItems: 'start',
+    borderRightWidth: 1,
+    borderRightColor: '#dddddd',
+  },
+  mainCompartmentTop: {
+    flex: 1,
+    width: '100%',
+    padding: 8,
+    // borderBottomWidth: 1,
+    // borderBottomColor: '#dddddd',
+  },
+  mainCompartmentBottom: {
+    flex: 1,
+    width: '100%',
+    padding: 8,
+  },
+  timerName: {
+    padding: 4,
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  listName: {
+    padding: 4,
+    fontSize: 20,
+    fontStyle: 'italic',
+  },
+  clockContainer: {
+    flex: 4,
     justifyContent: 'center',
     alignItems: 'center',
-    // padding: 16,
-    marginVertical: 8,
-    // backgroundColor: 'red',
-  },
-  row: {
-    flexDirection: 'row',
+    padding: 4,
   },
   clockface: {
     fontSize: 24,
@@ -127,6 +187,7 @@ const styles = StyleSheet.create({
     minWidth: 6,
     textAlign: 'center',
   },
-  
+  menuIconContainer: {
+    padding: 8,
+  },
 });
-// minWidth: 100,
