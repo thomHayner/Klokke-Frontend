c35cb0187afb0876cb3c8530f01d3bf54a2dfbb9
Thomas Hayner <thom.hayner@gmail.com>
2023-01-03 00:05:42 -0700

    placeholder for tags multiselect component

---
+++ components/screens/timers/TagsMultiSelect.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+// https://github.com/react-native-picker/picker
+import { Dropdown } from 'react-native-element-dropdown';
