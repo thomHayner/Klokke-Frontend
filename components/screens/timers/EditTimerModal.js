ea2e854d4e72ebb6615da0d5766513ee52f6f6d9
Thomas Hayner <thom.hayner@gmail.com>
2023-01-05 21:13:42 -0700

    build modal with some functionality

--- components/screens/timers/EditTimerModal.js
+++ components/screens/timers/EditTimerModal.js
@@ -1,43 +1,244 @@
 import * as React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { timersListState } from '../../../timers_recoil_state';
 import {
   Modal,
   Pressable,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
-// https://github.com/react-native-picker/picker
-import { Dropdown } from 'react-native-element-dropdown';
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets
+} from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
+import ListsDropdown from './ListsDropdown';
+import TagsMultiSelect from './TagsMultiSelect';
 
-export default function EditTimerModal({ modalVisible, toggleModal }) {
+const defaultTimer = {
+  id: -1,
+  name: '',
+  description: '',
+  list: '',
+  listPosition: false,
+  tags: [],
+  isRunning: false,
+  start: 0,
+  stop: 0,
+  elapsed: 0,
+  completed: false,
+};
+
+export default function EditTimerModal({
+  modalVisible,
+  toggleModal,
+  timer = defaultTimer,
+  scrollHandler,
+}) {
+  const insets = useSafeAreaInsets();
+  const setTimers = useSetRecoilState(timersListState);
+  const [nameValue, setNameValue] = React.useState(
+    timer.name ? timer.name : ''
+  );
+  const [listValue, setListValue] = React.useState(
+    timer.list ? timer.list : ''
+  );
+  const [tagsValue, setTagsValue] = React.useState(
+    timer.tags ? timer.tags : []
+  );
+  const [descriptionValue, setDescriptionValue] = React.useState(
+    timer.description ? timer.description : ''
+  );
   
+  const newTimer = {
+    id: timersListState.length,
+    name: nameValue,
+    description: descriptionValue,
+    list: listValue,
+    listPosition: false,
+    tags: tagsValue,
+    isRunning: false,
+    start: 0,
+    stop: 0,
+    elapsed: 0,
+    completed: false,
+  };
+
+  const addTimer = () => {
+    setTimers((oldTimersList) => [
+      ...oldTimersList,
+      newTimer
+    ]);
+    setNameValue('');
+    setListValue('');
+    setTagsValue('');
+    setDescriptionValue('');
+    setTimeout(()=> scrollHandler(), 100);
+  };
+
+  // const onChangeName = (name) => {
+  //   setNameValue(name);
+  // };
 
   return (
       <Modal
         animationType="slide"
-        // transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
           toggleModal();
         }}
       >
-        <View
-          style={{
-            flex:1,
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-          }}
-        >
-          <Pressable onPress={toggleModal}>
-            <Text>MODALMODALMODALMODAL</Text>
-          </Pressable>
-        </View>
+        <SafeAreaProvider>
+          <SafeAreaView style={styles.safeContainer}>
+            
+            <Pressable
+              style={[
+                  styles.closeButton,
+                  { top: insets.top + 10, right: insets.right + 15 }
+              ]}
+              onPress={toggleModal}
+            >
+              <MaterialIcons name='close' size={36} color='black' />
+            </Pressable>
+
+            <View style={styles.container}>
+              <Text style={styles.textLabel}>Name</Text>
+              <TextInput
+                style={styles.nameInput}
+                value={nameValue}
+                onChangeText={(name) => {setNameValue(name)}}
+              />
+            </View>
+
+            <View style={styles.container}>
+              <Text  style={styles.textLabel}>List</Text>
+              <ListsDropdown />
+            </View>
+
+            <View style={styles.container}>
+              <Text style={styles.textLabel}>Tags</Text>
+              <TagsMultiSelect />
+            </View>
+
+            <View style={styles.container}>
+              <Text style={styles.textLabel}>Intervals</Text>
+              {/* timer.start.map((n,i,a) => {}) */}
+              {/* map timer intervals, each interval needs to be editable */}
+              <View style={styles.row}>
+                <Text style={[styles.intervalLabel, { marginRight: 4 }]}>
+                  Start
+                </Text>
+                <Text style={[styles.intervalLabel, { marginLeft: 4 }]}>
+                  Stop
+                </Text>
+              </View>
+              <View style={styles.row}>
+                <TextInput
+                  style={[styles.intervalInput, { marginRight: 4 }]}
+                  value={''}
+                  onChangeText={{}}
+                />
+                <TextInput
+                  style={[styles.intervalInput, { marginLeft: 4 }]}
+                  value={''}
+                  onChangeText={{}}
+                />
+              </View>
+            </View>
+
+            <View style={[styles.row, { marginTop: 30, }]}>
+              <Pressable
+                style={[styles.buttonBorder, styles.submit]}
+                onPress={()=>{}}
+              >
+                <Text style={styles.buttonLabel}>Submit</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.buttonBorder, styles.cancel]}
+                onPress={toggleModal}
+              >
+                <Text style={styles.buttonLabel}>Cancel</Text>
+              </Pressable>
+            </View>
+
+          </SafeAreaView>
+        </SafeAreaProvider>
       </Modal>
     )
 };
 
 const styles = StyleSheet.create({
-
+  safeContainer: {
+    width: '100%',
+    alignItems: 'start',
+    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 32,
+  },
+  closeButton: {
+    position: 'absolute',
+  },
+  container: {
+    width: '100%',
+    alignItems: 'start',
+    justifyContent: 'center',
+    paddingVertical: 12,
+  },
+  textLabel: {
+    width: '100%',
+    paddingBottom: 4,
+    fontSize: 20,
+  },
+  nameInput: {
+    height: 40,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  intervalLabel: {
+    flex: 1,
+    paddingVertical: 4,
+  },
+  intervalInput: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  buttonBorder: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+  },
+  buttonLabel: {
+    fontSize: 16,
+  },
+  submit: {
+    marginRight: 16,
+    marginLeft: 8,
+    backgroundColor: 'green',
+  },
+  cancel: {
+    marginRight: 8,
+    marginLeft: 16,
+    backgroundColor: 'red', 
+  },
 });
+
+// 
