2e2317ba1ffd443db80bb14be94062954627203b
Thomas Hayner <thom.hayner@gmail.com>
2023-01-06 06:13:57 -0700

    add Stack navigator

--- components/screens/timers/TimersScreen.js
+++ components/screens/timers/TimersScreen.js
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Text, StyleSheet, View, } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View, } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import EditModal from './NavEditModal';
 import ListsDropdown from './ListsDropdown';
 import TimersList from './TimersList';
 
+const Stack = createStackNavigator();
+
 export default function TimersScreen() {
-  return (
+
+  const TimersHome = () => {
+    return (
     <View style={styles.safeAreaContainer}>
       <LinearGradient
         // Background Linear Gradient
@@ -23,6 +29,22 @@ export default function TimersScreen() {
       </View>
       <TimersList mode={'selectList'} />
     </View>
+    )
+  };
+
+  return (
+    <Stack.Navigator screenOptions={{ presentation: 'modal' }}>
+      <Stack.Screen
+        name='TimersScreen'
+        component={TimersHome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='TimersEditModal'
+        component={EditModal}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 };
 
