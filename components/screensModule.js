fbf093d7f45024b461df909e0ba2ad4ba11e1283
Thomas Hayner <thom.hayner@gmail.com>
2023-01-06 19:05:46 -0700

    add NavEditModal

--- components/screensModule.js
+++ components/screensModule.js
@@ -1,9 +1,11 @@
 import TimersScreen from './screens/timers/TimersScreen';
 import ReportsScreen from './screens/reports/ReportsScreen';
 import SettingsScreen from './screens/settings/SettingsScreen';
+import EditModal from './screens/timers/NavEditModal';
 
 export {
   TimersScreen,
   ReportsScreen,
   SettingsScreen,
+  EditModal,
 };
