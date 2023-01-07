2438c813e4e0f0e8cb2efec251e11e20f0d7a504
Thomas Hayner <thom.hayner@gmail.com>
2023-01-06 21:58:53 -0700

    cleanup

--- components/screens/timers/TimersScreen.js
+++ components/screens/timers/TimersScreen.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, View, } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import EditModal from './NavEditModal';
 import ListsDropdown from './ListsDropdown';
 import TimersList from './TimersList';
 
