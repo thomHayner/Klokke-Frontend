e40b9142bcff776971eb67d2d2a5b15fa05c893f
Thomas Hayner <thom.hayner@gmail.com>
2023-01-11 06:19:57 -0700

    update default states for better dev/testing

--- timers_recoil_state.js
+++ timers_recoil_state.js
@@ -6,10 +6,10 @@ const timersListState = atom({
     {
       id: 0,
       name: 'Timer #1',
-      description: '',
+      description: 'hello',
       list: 'My First List',
       listPosition: false,
-      tags: [],
+      tags: ['Work', 'Gym'],
       isRunning: false,
       start: 0,
       stop: 0,
@@ -28,7 +28,8 @@ const timersListState = atom({
     //   stop: 0,
     //   elapsed: 0,
     //   isCompleted: false,
-    // },{
+    // },
+    // {
     //   id: 2,
     //   name: 'timer_2',
     //   description: '',
@@ -40,7 +41,8 @@ const timersListState = atom({
     //   stop: 0,
     //   elapsed: 0,
     //   isCompleted: true,
-    // },{
+    // },
+    // {
     //   id: 3,
     //   name: 'timer_3',
     //   description: '',
@@ -52,7 +54,8 @@ const timersListState = atom({
     //   stop: 0,
     //   elapsed: 0,
     //   isCompleted: false,
-    // },{
+    // },
+    // {
     //   id: 4,
     //   name: 'timer_4',
     //   description: '',
@@ -77,9 +80,12 @@ const listsListState = atom({
   key: 'listsListState',
   default: [
     {
+      id: 0,
       label: 'All Timers',
       value: 'All',
-    },{
+    },
+    {
+      id: 1,
       label: 'My First List',
       value: 'My First List',
     },
@@ -95,22 +101,27 @@ const tagsListState = atom({
   key: 'tagsListState',
   default:[
     {
+      id: 0,
       label: 'Work',
       value: 'Work',
     },
     {
+      id: 1,
       label: 'Gym',
       value: 'Gym',
     },
     {
+      id: 2,
       label: 'Study',
       value: 'Study',
     },
     {
+      id: 3,
       label: 'Cooking',
       value: 'Cooking',
     },
     {
+      id: 4,
       label: 'Laundry',
       value: 'Laundry',
     },
