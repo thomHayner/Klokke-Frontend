f1811afa0da5993502cc88271fc5ef64a31306de
Thomas Hayner <thom.hayner@gmail.com>
2023-01-21 15:36:48 -0700

    create function to access database

--- sqlite_db.js
+++ sqlite_db.js
@@ -1,7 +1,8 @@
 import * as SQLite from 'expo-sqlite';
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
 
-export default function SQLiteProvider() {
-
+function SQLiteProvider() {
   if (Platform.OS === "web") {
     return {
       transaction: () => {
@@ -11,11 +12,26 @@ export default function SQLiteProvider() {
       }
     }
   };
-
-  const db = SQLite.openDatabase("klokkeLocalStorage.db");
+  const db = SQLite.openDatabase("klokkeDatabase.db");
   return db;
 };
 
-// This is an example of what goes in files which import SQLiteProvider()
-// import SQLiteProvider from 'sqlite_db';
-// const db = SQLiteProvider();
+async function openDatabase(pathToDatabaseFile) {
+  if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+    await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+  }
+  await FileSystem.downloadAsync(
+    Asset.fromModule(require(pathToDatabaseFile)).uri,
+    FileSystem.documentDirectory + 'SQLite/klokkeDatabase.db'
+  );
+  return SQLite.openDatabase('klokkeDatabase.db');
+}
+
+export {
+  SQLiteProvider,
+  openDatabase,
+}
+
+// This is an example of what goes in files which need to access the local db
+// import { openDatabase } from 'sqlite_db';
+// const db = openDatabase(pathToDatabaseFile[ i.e. ./assets/klokkeDatabase]);
